a2b01100ceb2a1cafc67b0b2407925aa3a706dbe
krystek01 <129667167+krystek01@users.noreply.github.com>
2024-02-03 19:23:47 +0100

    Add files via upload

---
+++ projekt/src/App.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        TS ts = new TS("gap1.txt");
+        ts.Start();
+        
+    }
+}
